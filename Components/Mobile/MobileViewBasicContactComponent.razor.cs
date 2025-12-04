66b7ce10e6dadc6cc1ae3423f23c6e164115738f
Andy Walter <musictopia2@hotmail.com>
2025-12-03 18:21:39 -0700

    Converted to using the new extension model in c#.

--- Components/Mobile/MobileViewBasicContactComponent.razor.cs
+++ Components/Mobile/MobileViewBasicContactComponent.razor.cs
@@ -1,5 +1,5 @@
 namespace ContactManager.CoreLibrary.Components.Mobile;
 public partial class MobileViewBasicContactComponent
 {
-    private static string GetDisplay(EnumPhoneCategory phone) => phone.ToString().GetWords();
+    private static string GetDisplay(EnumPhoneCategory phone) => phone.ToString().GetWords;
 }
